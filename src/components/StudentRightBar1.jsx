7dd886ba4b10fc9a5f354431f3698da075e9e642
adarsh57pr <adarshlp110@gmail.com>
2024-12-31 10:18:22 +0530

    3

--- src/components/StudentRightBar1.jsx
+++ src/components/StudentRightBar1.jsx
@@ -6,52 +6,57 @@ const StudentRightBar1 = (props) => {
   console.log(job)
 
   return (
-    <div className=' py-7'>
-      <div className="m-5 h-[600px] overflow-auto w-80 rounded-lg border-[15px] border-white  px-2 shadow-xl shadow-teal-300">
-        <p className="text-lg font-bold capitalize">{job.company}</p>
-        <img className='rounded' src={job.image} alt="" />
-        <p className="mt-3 text-4xl font-bold">{job.jobRole}</p>
-        <p className="text-sm font-semibold text-gray-500">{job.salary}</p>
-        {/* <button className="mt-4 w-full rounded-lg border-2 border-black px-10 py-2 text-sm font-semibold hover:bg-black hover:text-white">Request a free demo</button> */}
+    <div className=''>
+      <div className="m-5  overflow-auto w-96 rounded-lg px-5 py-3 shadow-xl shadow-teal-300">
+        <div className='mb-3 rounded-md pb-4 shadow-lg'>
+          <p className="my-2 text-3xl font-bold capitalize">{job.jobRole}</p>
+          <img className='rounded' src={job.image} alt="" />
+          <p className="text-lg font-bold underline capitalize">{job.company}</p>
+          <p className="text-sm font-semibold text-gray-700 ">&#8377; {job.salary} a month</p>
+          <p className="text-md font-semibold text-gray-900 mb-2 ">{job.location}</p>
 
+          <Link to='/applyJob' state={job} className='bg-blue-600 hover:bg-blue-800 px-6 py-2 rounded text-white'>Apply now</Link>
+        </div>
+        <div className='h-60 overflow-auto '>
+          <p className='text-lg font-medium'>Requirements</p>
+          {
+            job.requirements.map((ele) => {
+              return <li className="flex items-center space-x-4"><span className="h-2 w-2 ms-2 rounded-full bg-black" /><span>{ele}</span></li>
 
-        <p className='text-lg font-medium'>Requirements</p>
-        {
-          job.requirements.map((ele) => {
-            return <li className="flex items-center space-x-4"><span className="h-2 w-2 ms-2 rounded-full bg-black" /><span>{ele}</span></li>
+            })
+          }
+          <p className='text-lg font-medium'>Skills</p>
+          {
+            job.skills.map((ele) => {
+              return <li className="flex items-center space-x-4"><span className="h-2 w-2 ms-2 rounded-full bg-black" /><span>{ele}</span></li>
 
-          })
-        }
-        <p className='text-lg font-medium'>Skills</p>
-        {
-          job.skills.map((ele) => {
-            return <li className="flex items-center space-x-4"><span className="h-2 w-2 ms-2 rounded-full bg-black" /><span>{ele}</span></li>
+            })
+          }
 
-          })
-        }
+          <hr className="my-4" />
+          <ul className="space-y-2 font-semibold">
+            <li className="flex items-center space-x-2 text-rose-600">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth={2}>
+                <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
+              </svg>
+              <span>Everything from basic</span>
+            </li>
+            <li className="flex items-center space-x-2">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth={2}>
+                <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
+              </svg>
+              <span>24/7 Support</span>
+            </li>
+            <li className="flex items-center space-x-2">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth={2}>
+                <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
+              </svg>
+              <span>10 users</span>
 
-        <hr className="my-4" />
-        <ul className="space-y-2 font-semibold">
-          <li className="flex items-center space-x-2 text-rose-600">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth={2}>
-              <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
-            </svg>
-            <span>Everything from basic</span>
-          </li>
-          <li className="flex items-center space-x-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth={2}>
-              <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
-            </svg>
-            <span>24/7 Support</span>
-          </li>
-          <li className="flex items-center space-x-2">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-500" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth={2}>
-              <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
-            </svg>
-            <span>10 users</span>
-            <Link to='/applyJob' state={job} className='bg-black px-4 py-1 rounded text-white'>apply</Link>
-          </li>
-        </ul>
+            </li>
+          </ul>
+        </div>
+
       </div>
     </div>
   )
--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import axios from 'axios'
-
+import { Button, Modal } from 'antd';
 import StudentRightBar1 from '../../components/StudentRightBar1'
 
 
@@ -11,9 +11,9 @@ const Home = () => {
   let userStore = useSelector((state) => state.user)
   // console.log(userStore)
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
+  // const showModal = () => {
+  //   setIsModalOpen(true);
+  // };
   const handleOk = () => {
     setIsModalOpen(false);
   };
@@ -74,69 +74,71 @@ const Home = () => {
   return (
     <div className=''>
       <div className=' py-16 w-2/3'>
-     <div>
-     <div className='flex md:flex-row justify-center gap-4 flex-col mt-5 py-2 px-4 rounded-md w-max m-auto bg-slate-200'>
-        <input onChange={searchChanger} name='title' type="text" className='px-3 py-2 border-2 rounded-md' placeholder='search a job' />
-        <input onChange={searchChanger} name='location' type="text" className='px-3 py-2 border-2 rounded-md' placeholder='search a location' />
-        <button onClick={handleJobSubmit} className='bg-blue-700 hover:bg-blue-900 px-4 py-1 rounded-md  text-white'>search job</button>
-      </div>
-        {!clikedSearch && <div className='grid md:grid-cols-2 sm:grid-cols-2 '>
-          {
-            allJob?.jobs?.map((ele) => {
-              return <div key={ele._id} className=" bg-gray-100 w-96 mx-auto my-10 max-w-xs rounded-xl px-6 py-6 text-gray-700 shadow-xl shadow-teal-300">
-                <div>
-                  <img className='rounded h-40 w-80' src={ele.image} alt="" />
-                  <div className="my-4 w-max rounded-md bg-blue-900 px-2 py-1 text-sm text-center font-bold text-blue-100 capitalize">{ele.company}</div>
-                </div>
-                <p className="mb-2 text-2xl capitalize">{ele.title}</p>
-                <p className="mb-6 text-gray-600">{ele.description}</p>
-                <button onClick={() => handleViewJob(ele)} className="flex items-center space-x-2 rounded-md border-2 border-blue-500 px-4 py-2 font-medium text-blue-600 transition hover:bg-blue-500 hover:text-white">
-                  <span> Learn more </span>
-                  <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
-                    <path fillRule="evenodd" d="M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z" clipRule="evenodd" />
-                  </svg>
-                  </span>
-                </button>
-              </div>
-            })
-          }
-        </div>}
-        {clikedSearch &&
-          <div className='w-full flex flex-col items-center gap-7'>
+        <div>
+          <div className='flex md:flex-row justify-center gap-4 flex-col mt-5 py-2 px-4 rounded-md w-max m-auto bg-slate-200'>
+            <input onChange={searchChanger} name='title' type="text" className='px-3 py-2 border-2 rounded-md' placeholder='search a job' />
+            <input onChange={searchChanger} name='location' type="text" className='px-3 py-2 border-2 rounded-md' placeholder='search a location' />
+            <button onClick={handleJobSubmit} className='bg-blue-700 hover:bg-blue-900 px-4 py-1 rounded-md  text-white'>search job</button>
+          </div>
+          {!clikedSearch && <div className='grid md:grid-cols-2 sm:grid-cols-2 '>
             {
-              searchJobs.length > 0 ? searchJobs.map((ele, index) => {
-                return <div key={ele._id} className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md dark:bg-gray-800">
-                  <div className="flex items-center justify-between">
-                    <span className="text-sm  text-gray-800 dark:text-gray-400 capitalize font-bold">{ele.company}</span>
-                    <img className='w-16 h-16 rounded-full' src={ele.image} alt="" />
-                  </div>
+              allJob?.jobs?.map((ele) => {
+                return <div key={ele._id} className=" bg-gray-100 w-96 mx-auto my-10 max-w-xs rounded-xl px-6 py-6 text-gray-700 shadow-xl shadow-teal-300">
                   <div>
-                    <h1 className="mt-2 text-lg font-semibold text-gray-800 dark:text-white capitalize">{ele.title}</h1>
-                    <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">{ele.description}</p>
+                    <img className='rounded h-40 w-80' src={ele.image} alt="" />
+                    <div className="my-4 w-max rounded-md bg-blue-900 px-2 py-1 text-sm text-center font-bold text-blue-100 capitalize">{ele.company}</div>
                   </div>
-                  <div>
-                    <div className="flex items-center mt-2 text-gray-700 dark:text-gray-200">
-                      <span>Visit on:</span>
-                      <a className="mx-2 text-blue-600 cursor-pointer dark:text-blue-400 hover:underline" tabIndex={0} role="link">edx.org</a>
-                      <span>or</span>
-                      <a className="mx-2 text-blue-600 cursor-pointer dark:text-blue-400 hover:underline" tabIndex={0} role="link">classcentral.com</a>
-                    </div>
-                    <div className="flex items-center justify-center mt-4">
-                      <a className="mr-2 text-gray-800 cursor-pointer dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300" tabIndex={0} role="link" aria-label="twitter link">
-                        <button onClick={() => handleViewJob(ele)} className='border-b-2 pb-2'>view full job detail</button>
-                      </a>
-
-                    </div>
-                  </div>
+                  <p className="mb-2 text-2xl capitalize">{ele.title}</p>
+                  <p className="mb-6 text-gray-600">{ele.description}</p>
+                  <button onClick={() => handleViewJob(ele)} className="flex items-center space-x-2 rounded-md border-2 border-blue-500 px-4 py-2 font-medium text-blue-600 transition hover:bg-blue-500 hover:text-white">
+                    <span> Learn more </span>
+                    <span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-6 w-6">
+                      <path fillRule="evenodd" d="M16.72 7.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 010 1.06l-3.75 3.75a.75.75 0 11-1.06-1.06l2.47-2.47H3a.75.75 0 010-1.5h16.19l-2.47-2.47a.75.75 0 010-1.06z" clipRule="evenodd" />
+                    </svg>
+                    </span>
+                  </button>
                 </div>
-
-              }
-              )
-                : 'No match found related to this job'
+              })
             }
           </div>}
-     </div>
-        <div className='fixed top-16 right-10'> {show === true ? <StudentRightBar1 job={Viewjob} /> : ''}</div>
+          {clikedSearch &&
+            <div className='w-full flex flex-col items-center gap-7'>
+              {
+                searchJobs.length > 0 ? searchJobs.map((ele, index) => {
+                  return <div key={ele._id} className="w-full max-w-sm px-4 py-3 bg-white rounded-md shadow-md dark:bg-gray-800">
+                    <div className="flex items-center justify-between">
+                      <span className="text-sm  text-gray-800 dark:text-gray-400 capitalize font-bold">{ele.company}</span>
+                      <img className='w-16 h-16 rounded-full' src={ele.image} alt="" />
+                    </div>
+                    <div>
+                      <h1 className="mt-2 text-lg font-semibold text-gray-800 dark:text-white capitalize">{ele.title}</h1>
+                      <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">{ele.description}</p>
+                    </div>
+                    <div>
+                      <div className="flex items-center mt-2 text-gray-700 dark:text-gray-200">
+                        <span>Visit on:</span>
+                        <a className="mx-2 text-blue-600 cursor-pointer dark:text-blue-400 hover:underline" tabIndex={0} role="link">edx.org</a>
+                        <span>or</span>
+                        <a className="mx-2 text-blue-600 cursor-pointer dark:text-blue-400 hover:underline" tabIndex={0} role="link">classcentral.com</a>
+                      </div>
+                      <div className="flex items-center justify-center mt-4">
+                        <a className="mr-2 text-gray-800 cursor-pointer dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300" tabIndex={0} role="link" aria-label="twitter link">
+                          <button onClick={() => handleViewJob(ele)} className='border-b-2 pb-2'>view full job detail</button>
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                }
+                )
+                  : 'No match found related to this job'
+              }
+            </div>}
+        </div>
+        <div className='fixed top-12 right-10'>
+          {/* <Modal title="Basic Modal" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}> */}
+            {show === true ? <StudentRightBar1 job={Viewjob} /> : ''}
+          {/* </Modal> */}
+        </div>
       </div>
 
     </div>
